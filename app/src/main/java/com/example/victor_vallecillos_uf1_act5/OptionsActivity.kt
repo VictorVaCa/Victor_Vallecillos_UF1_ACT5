2e6ba3da61b6f0ec5674e6f453d9c7ce9b68c7b0
VictorVaCa <a17vicvalcam@inspedralbes.cat>
2024-10-21 11:36:44 +0200

    Practica Finalitzada (Sense probar)

--- app/src/main/java/com/example/victor_vallecillos_uf1_act5/OptionsActivity.kt
+++ app/src/main/java/com/example/victor_vallecillos_uf1_act5/OptionsActivity.kt
@@ -1,11 +1,16 @@
 package com.example.victor_vallecillos_uf1_act5
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class OptionsActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+
+        findViewById<Button>(R.id.menuButton).setOnClickListener {
+            finish()
+        }
     }
 }
